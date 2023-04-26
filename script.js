a34c4a013fe8ade51a25545b81fcc13aa9ac81c9
elmaccho <maciek.chojnacki22@wp.pl>
2023-04-26 23:24:15 +0200

    jeszcze troche skryptu

--- script.js
+++ script.js
@@ -2,21 +2,35 @@ const signupBtn = document.querySelector('.signupBtn')
 const loginBtn = document.querySelector('.loginBtn')
 
 const loginContainer = document.querySelector('.login-container')
-const loginBtnContainer = document.querySelector('.loginBtn-container')
+const loginBtnContainer = document.querySelector('.loginBtnContainer')
 
 const registerContainer = document.querySelector('.register-container')
-const registerBtnContainer = document.querySelector('.registerBtn-container')
+const registerBtnContainer = document.querySelector('.registerBtnContainer')
 
 const mainContainer = document.querySelector('.main-container')
 const btnContainer = document.querySelector('.btn-container')
 
 
 const switchToRegister = () => {
+    mainContainer.style.transform = "translateX(59%)"
+    btnContainer.style.transform = "translateX(-171%)"
 
+    registerBtnContainer.classList.toggle('hidden')
+    loginContainer.classList.toggle('hidden')
+
+    registerContainer.classList.toggle('hidden')
+    loginBtnContainer.classList.toggle('hidden')
 }
 
 const switchToLogin = () => {
+    mainContainer.style.transform = "translateX(0%)"
+    btnContainer.style.transform = "translateX(0%)"
 
+    registerBtnContainer.classList.toggle('hidden')
+    loginContainer.classList.toggle('hidden')
+
+    registerContainer.classList.toggle('hidden')
+    loginBtnContainer.classList.toggle('hidden')
 }
 
 signupBtn.addEventListener('click', switchToRegister)
