b8487a16c36223004650eef17831fdf0e05431dc
elmaccho <maciek.chojnacki22@wp.pl>
2023-04-26 23:11:30 +0200

    przebudowanie styli

---
+++ script.js
@@ -0,0 +1,23 @@
+const signupBtn = document.querySelector('.signupBtn')
+const loginBtn = document.querySelector('.loginBtn')
+
+const loginContainer = document.querySelector('.login-container')
+const loginBtnContainer = document.querySelector('.loginBtn-container')
+
+const registerContainer = document.querySelector('.register-container')
+const registerBtnContainer = document.querySelector('.registerBtn-container')
+
+const mainContainer = document.querySelector('.main-container')
+const btnContainer = document.querySelector('.btn-container')
+
+
+const switchToRegister = () => {
+
+}
+
+const switchToLogin = () => {
+
+}
+
+signupBtn.addEventListener('click', switchToRegister)
+loginBtn.addEventListener('click', switchToLogin)
